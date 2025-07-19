8a1379d04f2e60a6f644d08ebf98db8b7f100ee2
araf821 <araf.ahmed200@gmail.com>
2025-07-19 10:20:36 -0400

    feat: add Toaster component for notifications and integrate it into the layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inter, Oxanium } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${oxanium.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster position="top-center" />
         {children}
       </body>
     </html>
